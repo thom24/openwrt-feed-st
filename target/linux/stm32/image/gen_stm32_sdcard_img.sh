07c5492b03d14a53df0d77ba9247fdfab821bc33
Thomas Richard <thomas.richard@bootlin.com>
2024-07-02 09:06:55 +0200

    Revert "stm32: stm32mp2: add metadata partitions in the image"
    This reverts commit 3eb11f06317fd0d2d35feeddf183b37720387207.

--- target/linux/stm32/image/gen_stm32_sdcard_img.sh
+++ target/linux/stm32/image/gen_stm32_sdcard_img.sh
@@ -2,47 +2,37 @@
 # Copyright (C) 2024 Bootlin
 
 set -ex
-[ $# -eq 9 ] || {
-    echo "SYNTAX: $0 <file> <fsbl> <metadata> <fip> <bootfs image> <rootfs image> <env size> <bootfs size> <rootfs size>"
+[ $# -eq 8 ] || {
+    echo "SYNTAX: $0 <file> <fsbl> <fip> <bootfs image> <rootfs image> <env size> <bootfs size> <rootfs size>"
     exit 1
 }
 
 OUTPUT="${1}"
 FSBL="${2}"
-METADATA="${3}"
-FIP="${4}"
-BOOTFS="${5}"
-ROOTFS="${6}"
-ENVSIZE="$((${7} / 1024))"
-BOOTFSSIZE="${8}"
-ROOTFSSIZE="${9}"
+FIP="${3}"
+BOOTFS="${4}"
+ROOTFS="${5}"
+ENVSIZE="$((${6} / 1024))"
+BOOTFSSIZE="${7}"
+ROOTFSSIZE="${8}"
 
-set $(ptgen -o "${OUTPUT}" -g -a 5 -l 2048 -G ${GUID} -N fsbla1 -p 256K -N fsbla2 -p 256K -N metadata1 -t 0 -p 256K -N metadata2 -t 0 -p 256K -N fip-a -t 0 -p 3M -N fip-b -t 0 -p 3M -N u-boot-env -p "${ENVSIZE}" -N boot -t 83 -p${BOOTFSSIZE}M -N rootfs -t 83 -p ${ROOTFSSIZE}M)
+set $(ptgen -o "${OUTPUT}" -g -a 5 -l 2048 -G ${GUID} -N fsbl1 -p 2M -N fsbl2 -p 2M -N fip -p 2M -N u-boot-env -p "${ENVSIZE}" -N boot -p${BOOTFSSIZE}M -N rootfs -p ${ROOTFSSIZE}M)
 FSBL1OFFSET="$((${1} / 512))"
 FSBL1SIZE="$((${2} / 512))"
 FSBL2OFFSET="$((${3} / 512))"
 FSBL2SIZE="$((${4} / 512))"
-METADATA1OFFSET="$((${5} / 512))"
-METADATA1SIZE="$((${6} / 512))"
-METADATA2OFFSET="$((${7} / 512))"
-METADATA2SIZE="$((${8} / 512))"
-FIPAOFFSET="$((${9} / 512))"
-FIPASIZE="$((${10} / 512))"
-FIPBOFFSET="$((${11} / 512))"
-FIPBSIZE="$((${12} / 512))"
-ENVOFFSET="$((${13} / 512))"
-ENVSIZE="$((${14} / 512))"
-BOOTFSOFFSET="$((${15} / 512))"
-BOOTFSSIZE="$((${16} / 512))"
-ROOTFSOFFSET="$((${17} / 512))"
-ROOTFSSIZE="$((${18} / 512))"
+FIPOFFSET="$((${5} / 512))"
+FIPSIZE="$((${6} / 512))"
+ENVOFFSET="$((${7} / 512))"
+ENVSIZE="$((${8} / 512))"
+BOOTFSOFFSET="$((${9} / 512))"
+BOOTFSSIZE="$((${10} / 512))"
+ROOTFSOFFSET="$((${11} / 512))"
+ROOTFSSIZE="$((${12} / 512))"
 
 dd bs=512 if="${FSBL}" of="${OUTPUT}" seek="${FSBL1OFFSET}" conv=notrunc
 dd bs=512 if="${FSBL}" of="${OUTPUT}" seek="${FSBL2OFFSET}" conv=notrunc
-dd bs=512 if="${METADATA}" of="${OUTPUT}" seek="${METADATA1OFFSET}" conv=notrunc
-dd bs=512 if="${METADATA}" of="${OUTPUT}" seek="${METADATA2OFFSET}" conv=notrunc
-dd bs=512 if="${FIP}"  of="${OUTPUT}" seek="${FIPAOFFSET}" conv=notrunc
-dd bs=512 if="${FIP}"  of="${OUTPUT}" seek="${FIPBOFFSET}" conv=notrunc
+dd bs=512 if="${FIP}"  of="${OUTPUT}" seek="${FIPOFFSET}" conv=notrunc
 dd bs=512 if=/dev/zero of="${OUTPUT}" seek="${ENVOFFSET}" count="${ENVSIZE}" conv=notrunc
 dd bs=512 if="${BOOTFS}" of="${OUTPUT}" seek="${BOOTFSOFFSET}" conv=notrunc
 dd bs=512 if="${ROOTFS}" of="${OUTPUT}" seek="${ROOTFSOFFSET}" conv=notrunc
